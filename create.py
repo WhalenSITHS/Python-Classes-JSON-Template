e78c78fb08f77f8759ae903cc137bc3e9383f255
mr. whalen <michael.whalen@sitechhs.com>
2025-02-24 10:03:25 -0500

    sdfg

--- create.py
+++ create.py
@@ -3,11 +3,11 @@ import os
 ## Create Class for creating new dictionaries here
 
 
-with open("./json/data.json", "r") as f:
+with open("updated.json", "r") as f:
     # Serialize the updated Python list to a JSON string
     data = json.load(f)
     ##Call classes in here
-    data.append("aikshdf")
+    data.append("Hello Andy")
 
 
 
